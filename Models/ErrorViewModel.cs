decddd3e6d87587756949740b15562a4487ad885
nazadorno <naza.adorno@gmail.com>
2020-11-09 18:20:08 -0300

    Subo archivos del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _3F3R.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
